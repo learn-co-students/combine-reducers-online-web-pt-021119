77451b8d5fff6fc4088ec3f0715ba1f04ca5ffa7
github username <github email address>
2019-12-03 18:21:41 -0700

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,7 @@
-export const addAuthor = author => {
-  return {
-    type: 'ADD_AUTHOR',
-    author
-  };
-};
-
-export const removeAuthor = id => {
-  return {
-    type: 'REMOVE_AUTHOR',
-    id
-  };
-};
-
-export const addBook = book => {
-  return {
-    type: 'ADD_BOOK',
-    book
-  };
-};
-
-export const removeBook = id => {
-  return {
-    type: 'REMOVE_BOOK',
-    id
-  };
-};
+import { createStore } from "redux";
+import rootReducer from "./reducers/manageAuthorsAndBooks";
+ 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
